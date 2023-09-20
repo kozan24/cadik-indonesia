325fd8851f40c174ba12cb1796b7621f6c33798f
Qolby <yagami_qolby@Qolbys-MacBook-Pro.local>
2023-09-20 14:28:04 +0700

    web cadik

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Toggle Active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar side bar buat ngilangin nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
